b52ec02dddd2ba7edb0fc5712670a9aa3956868c
Ezra Peisach <ezra.peisach@rcsb.org>
2020-05-23 07:01:07 -0400

    jsbeautify sources to make easier to read

--- js/crrspndnc-hstry-stndaln.js
+++ js/crrspndnc-hstry-stndaln.js
@@ -8,31 +8,32 @@ JavaScript supporting wwPDB Messaging Module web interface -- "View All Correspo
 
 2016-07-06, RPS: Created
 *************************************************************************************************************/
-function handleSubmit(){
-	var depid = $('#identifier').val();
-	
-	if( depid && depid.length > 0 ){
-		$('#vw_crrspndnc').ajaxSubmit({url: '/service/messaging/verify_depid', async: true, clearForm: false,
-	        success: function(jsonData) {
-	        	if( jsonData ){
-					try{
-						if( jsonData.found == "y" ){
-							$('#vw_crrspndnc').get(0).setAttribute("action", "/service/messaging/view_correspondence");
-							$('#vw_crrspndnc').submit();
-						}else{
-							alert("Deposition ID is not recognized.");
-						}
-					}
-					catch(err){
-						alert("Problem processing deposition ID.");
-					}	        		
-	        	}
-	        }
-	    });
-	}
-	else{
-		alert("No Deposition ID specified.");
-	}
+function handleSubmit() {
+    var depid = $('#identifier').val();
+
+    if (depid && depid.length > 0) {
+        $('#vw_crrspndnc').ajaxSubmit({
+            url: '/service/messaging/verify_depid',
+            async: true,
+            clearForm: false,
+            success: function(jsonData) {
+                if (jsonData) {
+                    try {
+                        if (jsonData.found == "y") {
+                            $('#vw_crrspndnc').get(0).setAttribute("action", "/service/messaging/view_correspondence");
+                            $('#vw_crrspndnc').submit();
+                        } else {
+                            alert("Deposition ID is not recognized.");
+                        }
+                    } catch (err) {
+                        alert("Problem processing deposition ID.");
+                    }
+                }
+            }
+        });
+    } else {
+        alert("No Deposition ID specified.");
+    }
 }
 
 $("#vw_crrspndnc_submit").on("click", handleSubmit);
